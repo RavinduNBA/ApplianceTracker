90e3525f93d879fce0f1e0bc5992258d51a2eca8
RavinduNBA <57413176+RavinduNBA@users.noreply.github.com>
2024-12-09 19:57:26 +0530

    Splash Screen Animation

--- app/src/main/java/com/netceylon/appliancetracker/MainActivity.kt
+++ app/src/main/java/com/netceylon/appliancetracker/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.netceylon.appliancetracker
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var topAnim: Animation
+    private lateinit var bottomAnim: Animation
+    private lateinit var image: ImageView
+    private lateinit  var logo: TextView
+    private lateinit var slogan: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation)
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation)
+
+        //Hooks
+        image = findViewById(R.id.imageView)
+        logo = findViewById(R.id.textView)
+        slogan = findViewById(R.id.textView2)
+        button = findViewById(R.id.button)
+
+        image.animation = topAnim
+        logo.animation = bottomAnim
+        slogan.animation = bottomAnim
+        button.animation = bottomAnim
+
+
     }
 }
